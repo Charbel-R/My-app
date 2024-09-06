28b2bc6076de26d8901e98a0755ffabfa247d476
Charbel Rouhana <charbelmrouhana@gmail.com>
2024-09-06 22:23:57 +0200

    chore: Add error handling to review page and create error component Try again button to recover from Error

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
 import { notFound } from "next/navigation";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+};
 
 export default function Review( {
   params,
@@ -8,12 +14,17 @@ export default function Review( {
     reviewId : string;
   };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error Loading Review");
+  }
+  
   if (parseInt(params.productId) > 10 || parseInt(params.reviewId) > 10) {
     return notFound();
   }
+
   return (
-    <h1>
-      Review {params.reviewId} for Product {params.productId}
-    </h1>
+    <h1> Review {params.reviewId} for Product {params.productId} </h1>
   )
 }
